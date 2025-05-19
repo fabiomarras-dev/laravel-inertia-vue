9c0a82ab5628a98d7c9a5f59dbfb92b53674b0a6
todoketee <fabio-marras@hotmail.com>
2025-05-19 10:16:30 +0200

    switching to form components

--- laravel-inertia-vue/resources/js/Pages/Auth/Register.vue
+++ laravel-inertia-vue/resources/js/Pages/Auth/Register.vue
@@ -2,7 +2,8 @@
 //import { reactive } from 'vue';
 //import { router } from '@inertiajs/vue3';
 import { useForm } from '@inertiajs/vue3';
-
+import { route } from '../../../../vendor/tightenco/ziggy/src/js';
+import TextInput from "../Components/TextInput.vue";
 
 /* const form = reactive({
     name: null,
@@ -22,7 +23,7 @@ const submit =() => {
     //console.log(form);
     //router.post('/register', form);
 
-    form.post('/register', {
+    form.post(route('register'), {
         onError: () => form.reset('password', 'password_confirmation'),
     });
 }
@@ -38,28 +39,57 @@ const submit =() => {
     <div class="w-2/4 mx-auto">
         <form @submit.prevent="submit">
 
-            <div class="mb-6">
+            <TextInput 
+                name="name" 
+                v-model="form.name" 
+                :message="form.errors.name"
+             />
+
+            <!-- <div class="mb-6">
                 <label>Name</label>
                 <input type="text" v-model="form.name"/>
                 <small>{{ form.errors.name }}</small>
-            </div>
+            </div> -->
 
-            <div class="mb-6">
+            <TextInput 
+                name="email" 
+                type="email"
+                v-model="form.email" 
+                :message="form.errors.email"
+             />
+
+            <!-- <div class="mb-6">
                 <label>Email</label>
                 <input type="text" v-model="form.email"/>
                 <small>{{ form.errors.email }}</small>
-            </div>
+            </div> -->
 
-            <div class="mb-6">
+            <TextInput 
+                name="password" 
+                type="password"
+                v-model="form.password" 
+                :message="form.errors.password"
+             />
+
+            <!-- <div class="mb-6">
                 <label>Password</label>
                 <input type="password" v-model="form.password"/>
                 <small>{{ form.errors.password }}</small>
-            </div>
+            </div> -->
 
-            <div class="mb-6">
+            <TextInput 
+                name="confirm password" 
+                type="password"
+                v-model="form.name" 
+             />
+
+            <!-- <div class="mb-6">
                 <label>Confirm Password</label>
                 <input type="password" v-model="form.password_confirmation" />
-            </div>
+            </div> -->
+
+
+
 
             <div>
                 <p class="text-slate-600 mb-2">Already a user? <a class="text-link">Login</a></p>
