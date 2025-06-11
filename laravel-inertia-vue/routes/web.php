78fd615c70f8d52de865eb414120bec8d5c56d32
todoketee <fabio-marras@hotmail.com>
2025-06-11 10:01:53 +0200

    auth

--- laravel-inertia-vue/routes/web.php
+++ laravel-inertia-vue/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 //use Inertia\Inertia;
 
@@ -20,6 +21,10 @@ Route::get('/', function(Request $request){
         })->paginate(5)->withQueryString(),
 
         'searchTerm' => $request->search,
+
+        'can' => [
+            'delete_user' => Auth::user() ? Auth::user()->can('delete', User::class) : null
+        ]
     ]);
 })->name('home');
 
